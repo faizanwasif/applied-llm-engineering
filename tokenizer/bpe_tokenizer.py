fda21b069206fbe830b38d6088af7fa21e0c546d
faizanwasif <faizanwn102@gmail.com>
2026-08-20 14:08:25 +0500

    bpe tokenizer

---
+++ tokenizer/bpe_tokenizer.py
@@ -0,0 +1,63 @@
+from collections import defaultdict
+
+def learn_bpe(corpus, num_merges=3):
+    vocab = defaultdict(int)
+    
+   
+    for sentence in corpus.split('.'):
+        words = sentence.strip().split()
+        for word in words:
+            chars = ['<'] + list(word) + ['>']  
+            for i in range(len(chars) - 1):
+                pair = (chars[i], chars[i+1])
+                vocab[pair] += 1
+
+    merges = []
+    for _ in range(num_merges):
+        if not vocab:
+            break
+        
+        most_frequent = max(vocab, key=lambda x: vocab[x])
+        merges.append(most_frequent)
+
+       
+        new_char = ''.join(most_frequent)
+        new_vocab = defaultdict(int)
+        for pair in vocab:
+            count = vocab[pair]
+            if pair == most_frequent:
+                continue
+            new_pair = list(pair)
+            if new_pair[0] == most_frequent[0] and new_pair[1] == most_frequent[1]:
+                new_pair[0] = new_char
+                new_pair.pop(1)
+            new_vocab[tuple(new_pair)] += count
+        vocab = new_vocab
+    return merges
+
+def apply_bpe(text, merges):
+    chars = ['<'] + list(text) + ['>']  
+    for merge in reversed(merges):  
+        merged = ''.join(merge)
+        new_chars = []
+        i = 0
+        while i < len(chars) - 1:
+            if (chars[i], chars[i+1]) == merge:  
+                new_chars.append(merged)
+                i += 2
+            else:
+                new_chars.append(chars[i])
+                i += 1
+        if i < len(chars):
+            new_chars.append(chars[-1])
+        chars = new_chars
+    
+    return chars
+
+
+# Example usage
+corpus = "samalamadumalama"
+merges = learn_bpe(corpus, num_merges=3) 
+print("Learned Merges:", merges)
+bpe_representation = apply_bpe(corpus, merges)
+print(f"BPE Representation for {corpus}:", bpe_representation)
